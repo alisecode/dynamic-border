ff07a33dc0cca51f96e173c725da5942be860532
Alisa <alisa.melissa@icloud.com>
2024-03-12 10:31:24 +0200

    Initial Commit

---
+++ GradientCard/GradientCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientCardApp.swift
+//  GradientCard
+//
+//  Created by Алиса Третьякова on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GradientCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
